ce259792fd359c3a183ee0a8ca1ef57eb2aab196
Konstantin Gassmann <kg@creemedia.com>
2020-04-18 10:54:23 +0200

    add geometry

---
+++ geometry.ts
@@ -0,0 +1,87 @@
+type TRect = {
+  x: number;
+  y: number;
+  z: number;
+  width: number;
+  height: number;
+  subX?: number;
+  subY?: number;
+};
+
+export const rect = ({ x, y, z, width, height, subX = 1, subY = 1 }: TRect) => {
+  const stepX = width / subX;
+  const stepY = height / subY;
+
+  const positions = [];
+
+  const yEnd = y + stepY;
+  const xEnd = x + stepX;
+
+  for (let j = 0; j < height; j += stepY) {
+    for (let i = 0; i < width; i += stepX) {
+      // one quad made of two triangles
+      positions.push(
+        x + i,
+        y + j,
+        z,
+
+        xEnd + i,
+        y + j,
+        z,
+
+        x + i,
+        yEnd + j,
+        z,
+
+        x + i,
+        yEnd + j,
+        z,
+
+        xEnd + i,
+        y + j,
+        z,
+
+        xEnd + i,
+        yEnd + j,
+        z
+      );
+    }
+  }
+
+  const uvstepX = 1 / subX;
+  const uvstepY = 1 / subY;
+
+  const uv = [];
+
+  for (let j = 0; j < 1; j += uvstepY) {
+    for (let i = 0; i < 1; i += uvstepX) {
+      uv.push(
+        i,
+        j,
+        0,
+
+        uvstepX + i,
+        j,
+        0,
+
+        i,
+        uvstepY + j,
+        0,
+
+        i,
+        uvstepY + j,
+        0,
+
+        uvstepX + i,
+        j,
+        0,
+
+        uvstepX + i,
+        uvstepY + j,
+        0
+      );
+    }
+  }
+
+  return [new Float32Array(positions), new Float32Array(uv)];
+};
